4712d0e0efca34e5f3c6b9d86e17a321b7fd477e
Derek Barbosa <debarbos@redhat.com>
2025-09-23 14:45:52 -0400

    scripts: fix adopt script
    Signed-off-by: Derek Barbosa <debarbos@redhat.com>

--- adopt.sh
+++ adopt.sh
@@ -7,9 +7,9 @@ email_domain=$4
 
 set -e
 
-find ./src/ -type f | while IFS= read -r line; do 
-	sed -i "s/Jerry/$firstname/g" $line; 
-	sed -i "s/Snitselaar/$surname/g" $line;
-	sed -i "s/jsnitsel/$email/g" $line;
-	sed -i "s/@redhat.com/$email_domain/g" $line;
+find ./src/* -type f | while IFS= read -r line; do
+	sed -i "s/Foo/$firstname/g" $line;
+	sed -i "s/Bar/$surname/g" $line;
+	sed -i "s/user/$email/g" $line;
+	sed -i "s/@foobar.com/$email_domain/g" $line;
 done
